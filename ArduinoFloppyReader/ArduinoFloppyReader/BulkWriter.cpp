2e0611ef857a74d7e841071ea9be3ee0e1c62c71
JanMartens <jnmartens@telenet.be>
2021-11-11 12:16:38 +0100

    First version

--- ArduinoFloppyReader/ArduinoFloppyReader/BulkWriter.cpp
+++ ArduinoFloppyReader/ArduinoFloppyReader/BulkWriter.cpp
@@ -1,10 +1,10 @@
 #include "BulkWriter.h"
-//#include <synchapi.h>
 
 #include "ArduinoInterface.h"
 
 #include <iostream>
 #include <filesystem>
+#include <thread>
 
 namespace fs = std::filesystem;
 
@@ -16,52 +16,52 @@ namespace fs = std::filesystem;
 #include <stdio.h>
 #include <termios.h>
 
- //#ifndef _wcsupr
- //#include <wctype.h>
- //void _wcsupr(wchar_t* str) {
- //    while (*str) {
- //	   *str = towupper(*str);
- //	   str++;
- //    }
- //}
- //#endif
+//#ifndef _wcsupr
+//#include <wctype.h>
+//void _wcsupr(wchar_t* str) {
+//    while (*str) {
+//	   *str = towupper(*str);
+//	   str++;
+//    }
+//}
+//#endif
 
 static struct termios old, current;
 
 /* Initialize new terminal i/o settings */
 void BulkWriter::initTermios(int echo)
 {
-    tcgetattr(0, &old); /* grab old terminal i/o settings */
-    current = old; /* make new settings same as old settings */
-    current.c_lflag &= ~ICANON; /* disable buffered i/o */
-    if (echo) {
-	   current.c_lflag |= ECHO; /* set echo mode */
-    }
-    else {
-	   current.c_lflag &= ~ECHO; /* set no echo mode */
-    }
-    tcsetattr(0, TCSANOW, &current); /* use these new terminal i/o settings now */
+	tcgetattr(0, &old); /* grab old terminal i/o settings */
+	current = old; /* make new settings same as old settings */
+	current.c_lflag &= ~ICANON; /* disable buffered i/o */
+	if (echo) {
+		current.c_lflag |= ECHO; /* set echo mode */
+	}
+	else {
+		current.c_lflag &= ~ECHO; /* set no echo mode */
+	}
+	tcsetattr(0, TCSANOW, &current); /* use these new terminal i/o settings now */
 }
 
 /* Restore old terminal i/o settings */
 void BulkWriter::resetTermios(void)
 {
-    tcsetattr(0, TCSANOW, &old);
+	tcsetattr(0, TCSANOW, &old);
 }
 
 char BulkWriter::_getChar()
 {
-    char ch;
-    initTermios(0);
-    ch = getchar();
-    resetTermios();
-    return ch;
+	char ch;
+	initTermios(0);
+	ch = getchar();
+	resetTermios();
+	return ch;
 }
 
 std::wstring BulkWriter::atw(const std::string& str) {
-    std::wstring ws(str.size(), L' ');
-    ws.resize(::mbstowcs((wchar_t*)ws.data(), str.c_str(), str.size()));
-    return ws;
+	std::wstring ws(str.size(), L' ');
+	ws.resize(::mbstowcs((wchar_t*)ws.data(), str.c_str(), str.size()));
+	return ws;
 }
 #endif
 
@@ -69,22 +69,20 @@ using namespace ArduinoFloppyReader;
 
 // Read an ADF file and write it to disk
 int BulkWriter::adf2Disk(const std::wstring& filename, bool verify) {
-	printf("\nWrite disk from ADF mode\n\n");
-	if (!verify) printf("WARNING: It is STRONGLY recommended to write with verify support turned on.\r\n\r\n");
-
 	bool hdMode = false;
 
 	// Detect disk speed
 	const ArduinoFloppyReader::FirmwareVersion v = m_adfWriter.getFirwareVersion();
 
-	if (((v.major == 1) && (v.minor >= 9)) || (v.major > 1)) {
-		if (m_adfWriter.GuessDiskDensity(hdMode) != ArduinoFloppyReader::ADFResult::adfrComplete) {
-			printf("Unable to work out the density of the disk inserted.\n");
-			return 1;
-}
-	}
+	// Disabled this for the moment, seems to be failing from time to time.
+	//if (((v.major == 1) && (v.minor >= 9)) || (v.major > 1)) {
+	//	if (m_adfWriter.GuessDiskDensity(hdMode) != ArduinoFloppyReader::ADFResult::adfrComplete) {
+	//		printf("Unable to work out the density of the disk inserted.\n");
+	//		return 1;
+	//	}
+	//}
 
-	ADFResult result = m_adfWriter.ADFToDisk(filename, hdMode, verify, true, false, true, [](const int currentTrack, const DiskSurface currentSide, bool isVerifyError, const CallbackOperation operation) ->WriteResponse {
+	ADFResult result = m_adfWriter.ADFToDisk(filename, hdMode, verify, true, false, true, [&](const int currentTrack, const DiskSurface currentSide, bool isVerifyError, const CallbackOperation operation) ->WriteResponse {
 		if (isVerifyError) {
 			char input;
 			do {
@@ -102,7 +100,7 @@ int BulkWriter::adf2Disk(const std::wstring& filename, bool verify) {
 			case 'A': return WriteResponse::wrAbort;
 			}
 		}
-		printf("\rWriting Track %i, %s side     ", currentTrack, (currentSide == DiskSurface::dsUpper) ? "Upper" : "Lower");
+		printf("\r  Writing Track %i, %s side     ", currentTrack, (currentSide == DiskSurface::dsUpper) ? "Upper" : "Lower");
 #ifndef _WIN32
 		fflush(stdout);
 #endif		
@@ -110,7 +108,9 @@ int BulkWriter::adf2Disk(const std::wstring& filename, bool verify) {
 		});
 
 	switch (result) {
-	case ADFResult::adfrComplete:					printf("\rADF file written to disk                                                           "); break;
+	case ADFResult::adfrComplete:					
+		//printf("\rADF file written to disk                                                           "); 
+		break;
 	case ArduinoFloppyReader::ADFResult::adfrExtendedADFNotSupported:	printf("\rExtended ADF files are not currently supported.                                    "); break;
 	case ArduinoFloppyReader::ADFResult::adfrMediaSizeMismatch:			if (hdMode)
 		printf("\rDisk in drive was detected as HD, but a DD ADF file supplied.                      "); else
@@ -139,100 +139,131 @@ int BulkWriter::adf2Disk(const std::wstring& filename, bool verify) {
 // Iterate through a directory
 void BulkWriter::writeDirectory(const std::wstring& port, const std::wstring& directory, const bool inHDMode, bool verify) {
 
-    std::string const& adfExtension = ".adf";
+	std::string const& adfExtension = ".adf";
 
-    if (!fs::exists(directory))
-    {
-	   std::wcout << "Directory not found " << directory << std::endl;
-	   return;
-    }
+	if (!fs::exists(directory))
+	{
+		std::wcout << "Directory not found " << directory << std::endl;
+		return;
+	}
 
-    if (!m_adfWriter.openDevice(port)) {
-	   printf("\rError opening COM port: %s  ", m_adfWriter.getLastError().c_str());
-	   return;
-    }
+	if (!m_adfWriter.openDevice(port)) {
+		printf("\rError opening COM port: %s  ", m_adfWriter.getLastError().c_str());
+		return;
+	}
 
 
-    bool isFirstDisk = true;
+	bool isFirstDisk = true;
 
-    std::wcout << "Writing all adf files in folder: " << directory << std::endl;
+	auto iterator = fs::directory_iterator(directory);
 
-    for (const auto& entry : fs::directory_iterator(directory))
-    {
-	   if (!isFirstDisk)
-	   {
-		  std::cout << std::endl;
-		  std::cout << "Remove disk, and insert next disk." << std::endl;
+	std::wcout << "Writing all adf files in folder: " << directory << std::endl << std::endl;
 
-		  while (m_adfWriter.DiskPresent())
-		  {
-			// TODO: replace this
-			//  Sleep(500);
-		  }
 
-	   }
-	   else
-	   {
-		  isFirstDisk = false;
-	   }
+	for (const auto& entry : iterator)
+	{
+		if (!isFirstDisk)
+		{
+			std::cout << "\rRemove disk, and insert next disk.        ";
+			while (m_adfWriter.DiskPresent())
+			{
+				std::this_thread::sleep_for(std::chrono::milliseconds(500));
+			}
+		}
+		else
+		{
+			if (!m_adfWriter.DiskPresent())
+			{
+				std::cout << "Insert first disk.";
+			}
+			isFirstDisk = false;
+		}
 
-	   std::cout << "Insert not write protected disk." << std::endl;
+		bool isWriteProtected = false;
+		bool isDiskPresent = false;
+		bool writeProtectWarningGiven = false;
 
-	   bool isWriteProtected = false;
-	   bool isDiskPresent = false;
-	   bool writeProtectWarningGiven = false;
+		bool checkForNonWriteProtectedDisk = true;
 
-	   bool checkForNonWriteProtectedDisk = true;
+		while (checkForNonWriteProtectedDisk)
+		{
+			isDiskPresent = m_adfWriter.DiskPresent();
+			isWriteProtected = m_adfWriter.WriteProtected();
 
-	   while (checkForNonWriteProtectedDisk)
-	   {
-		  isDiskPresent = m_adfWriter.DiskPresent();
-		  isWriteProtected = m_adfWriter.WriteProtected();
-
-		  if (isDiskPresent && !isWriteProtected)
-		  {
-			 // Everything ok, get ready to start writing.
-			 checkForNonWriteProtectedDisk = false;
-		  }
-		  else
-		  {
-			 if (isDiskPresent && isWriteProtected)
-			 {
-				if (!writeProtectWarningGiven)
+			if (isDiskPresent && !isWriteProtected)
+			{
+				// Everything ok, get ready to start writing.
+				checkForNonWriteProtectedDisk = false;
+			}
+			else
+			{
+				if (isDiskPresent && isWriteProtected)
 				{
-				    std::cout << "Disk is write protected." << std::endl;
-				    writeProtectWarningGiven = true;
+					if (!writeProtectWarningGiven)
+					{
+						std::cout << "\rDisk is write protected, insert not write protected disk.         " ;
+						writeProtectWarningGiven = true;
+					}
 				}
-			 }
-			// TODO: remove this
-			//  Sleep(2000);
-		  }
-	   }
 
+				std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+			}
+		}
 
-	   if (entry.is_regular_file() && entry.path().extension() == adfExtension)
-	   {
-		  std::cout << "Writing : " << std::endl;
-		  std::cout << entry.path().filename() << std::endl;
+		if (entry.is_regular_file() && entry.path().extension() == adfExtension)
+		{
+			std::cout << "\r-> " << entry.path().filename() << std::endl;
 
-		  if (entry.exists())
-		  {
+			if (entry.exists())
+			{
+				bool writtenSuccesfullyOrAborted = false;
+
+				do {
 #ifdef _WIN32
-              adf2Disk(entry.path(), verify);
+				int result = adf2Disk(entry.path(), verify);
 #else
-              adf2Disk(atw(entry.path()), inHDMode, verify);
+				int result = adf2Disk(atw(entry.path()), verify);
 #endif
-		  }
-		  else
-		  {
-			 std::cout << "Error opening " << entry.path().filename() << std::endl;
-		  }
-	   }
-    }
+					if (result)
+					{
+						std::cout << "Something went wrong. [R]etry, [S]kip, [A]bort?";
 
-    std::wcout << "Finished writing files in folder: " << directory << std::endl;
+						char input;
 
-    //TODO: add some better handling for this => possibly opening and closing for every disk.
-    m_adfWriter.closeDevice();
+						do {
+#ifdef _WIN32
+							input = toupper(_getch());
+#else
+							input = toupper(_getChar());
+#endif	
+						} while ((input != 'R') && (input != 'S') && (input != 'A'));
+
+						switch (input) {
+						case 'R':
+							break;
+						case 'S':
+							writtenSuccesfullyOrAborted = true;
+							;
+						case 'A':
+							m_adfWriter.closeDevice();
+							return;
+						}
+					}
+					else
+					{
+						writtenSuccesfullyOrAborted = true;
+					}
+
+				} while (!writtenSuccesfullyOrAborted);
+			}
+			else
+			{
+				std::cout << "Error opening " << entry.path().filename() << std::endl;
+			}
+		}
+	}
+
+	//TODO: add some better handling for this => possibly opening and closing for every disk.
+	m_adfWriter.closeDevice();
 
 }
--- ArduinoFloppyReader/ArduinoFloppyReader/BulkWriter.h
+++ ArduinoFloppyReader/ArduinoFloppyReader/BulkWriter.h
@@ -1,5 +1,3 @@
-#pragma once
-
 #include "ADFWriter.h"
 
 class BulkWriter
